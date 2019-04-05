8ee293c38ba8bf7ebf20a80cc39e59a9d02e8572
Emrullah <emrullahayin@gmail.com>
2019-04-05 21:05:50 +0300

    .

--- components/UserForm.js
+++ components/UserForm.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 
-export class UserForm extends Component {
+class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  };
+
+  stepAction = event => {
+    const { step } = this.state;
+    let stepValue = event === "next" ? step + 1 : step - 1;
+    this.setState({
+      step: stepValue
+    });
+  };
+
   render() {
-    return <div />;
+    return <button onClick={this.stepAction("next")}>Next</button>;
   }
 }
 
