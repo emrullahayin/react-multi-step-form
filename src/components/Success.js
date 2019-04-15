96a780ac2ac48ab2fa4cfb09efe724e57a6817c3
Emrullah Ayın <e.ayin@jollytur.com>
2019-04-15 14:10:11 +0300

    success ui

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,10 +1,16 @@
 import React, { Component, Fragment } from "react";
+import Typography from "@material-ui/core/Typography";
+
 export class Success extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Thank You For Your Submission</h1>
-        <p>You will get an email with further instructions</p>
+        <Typography  style={{ marginTop: 15, width: '100%' }} component="h4" variant="h3" gutterBottom>
+          Thank You For Your Submission
+        </Typography>
+        <Typography component="p" variant="h6">
+          You will get an email with further instructions
+        </Typography>
       </Fragment>
     );
   }
